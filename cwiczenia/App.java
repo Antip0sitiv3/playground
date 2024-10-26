29e57a8a1806dc8cb04c5369184ea1626ff851ce
ktabaka <krystian@tabaka.it>
2024-10-26 11:00:16 +0200

    Wykonanie cwiczenia "zagniezdzenie petli w petli" w ramach cwiczen z zajac "Jezyki i paradygmaty programowania"

--- cwiczenia/App.java
+++ cwiczenia/App.java
@@ -1,22 +1,39 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Aplikacja sprawdzająca czy podana liczba jest liczbą parzystą lub nieparzystą.");
+        System.out.println("Aplikacja do nauki obslugi bledow oraz petli.");
 
         int number;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Podaj liczbę: ");
-        number = input.nextInt();
+
+        try {
+            number = input.nextInt();
+
+            for (int j = 0; j < number; j++) {
+                for (int i = 0; i <= number; i++) {
+                    if (i < number) {
+                        System.out.print(i + ", ");
+                    } else {
+                        System.out.print(i);
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (InputMismatchException exception) {
+            System.out.println("Proszę podać liczbę całkowitą");
+        }
         input.close();
 
-        if (number % 2 == 0) {
-            System.out.println("Podana liczba jest parzysta.");
-        }
-        else {
-            System.out.println("Podana liczba jest nieparzysta.");
-        }
+//        if (number % 2 == 0) {
+//            System.out.println("Podana liczba jest parzysta.");
+//        } else {
+//            System.out.println("Podana liczba jest nieparzysta.");
+//        }
 
 
     }
